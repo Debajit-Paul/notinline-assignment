ca84c6f2d616de8abb97a3dd608d1d390089bf19
Debajit Paul <debajitfan.of@gmail.com>
2023-08-09 23:41:49 -0700

    minor image change

--- src/data/ExpandCardData.js
+++ src/data/ExpandCardData.js
@@ -1,25 +1,29 @@
+import Icon_1 from "../assets/Icon_1.svg";
+import Icon_2 from "../assets/Icon_2.svg";
+import Icon_3 from "../assets/Icon_3.svg";
+import Icon_4 from "../assets/Icon_4.svg";
 const ExpandCardData = [
   {
     id: 1,
-    img: "./public/Icon_1.svg",
+    img: `${Icon_1}`,
     title: "NABL Accredited Labs",
     dec: "Get accurate and safe results from our NABL-certified lab partners.",
   },
   {
     id: 2,
-    img: "./public/Icon_2.svg",
+    img: `${Icon_2}`,
     title: "Timely collections",
     dec: "We collect samples timely to prevent treatment delays and ensure your continued well-being.",
   },
   {
     id: 3,
-    img: "./public/Icon_3.svg",
+    img: `${Icon_3}`,
     title: "Timely collections",
     dec: "We collect samples timely to prevent treatment delays and ensure your continued well-being.",
   },
   {
     id: 4,
-    img: "./public/Icon_4.svg",
+    img: `${Icon_4}`,
     title: "Transparency",
     dec: "Enjoy hassle free medical assistance with our transparent approach. We prioritize transparency for your smooth medical journey.",
   },
